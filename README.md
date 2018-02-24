4cbd23af8ef660abd31c2aa1948d5dc7ee18be80
MikSDigital <mixa.ru.ru@gmail.com>
2018-02-24 13:31:43 +0200

    Migration for User entity

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ LOGIN FORM test , Symfony Standard Edition, v.3.4.5
 ##### Create User class 
 
 * create Entity User which implements Userinterface (it is not required to use User class as an entity, just optional for storing users in DB)
-* 
+* add private $id and $email properties
+* implement ```getUsername()``` and ```getRoles()``` methods
+* add getter and setter for $email property
+* add Doctrine ORM mapping annotations
+* install Doctrine migration bundle : ```composer require doctrine/doctrine-migrations-bundle```
+* generate migration ```bin/console doctrine:migrations:diff```
 
 
 
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,19 +2,43 @@
 
 namespace AppBundle\Entity;
 
-
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
 class User implements UserInterface
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
     public function getUsername()
     {
-        // TODO: Implement getUsername() method.
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
     }
 
     public function getRoles()
     {
-        // TODO: Implement getRoles() method.
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
